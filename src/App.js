a81ebe2ea5ab759782468264e736edee63ba78a9
Anurag <amayekar82@gmail.com>
2021-10-18 01:31:49 +0530

    responsive infinite scroll list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useState, useRef, useCallback } from "react";
+import useDocumentList from "./Hooks/useDocumentList";
+import logo from "./logo.svg";
 import './App.css';
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [pageNum, setPageNum] = useState(1);
+
+  const {
+    hasMoreDocs,
+    isLoading,
+    error,
+    documents
+  } = useDocumentList(pageNum, query);
+
+  const observer = useRef();
+  const lastDocument = useCallback(doc => {
+    if(isLoading) return;
+    if(observer.current) {
+      observer.current.disconnect();
+    }
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMoreDocs) {
+        setPageNum(prevNum => prevNum + 1);
+      }
+    })
+    if(doc) {
+      observer.current.observe(doc);
+    }
+  }, [isLoading, hasMoreDocs]);
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+    setPageNum(1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <h2 className="heading"> Keep Searching... </h2>
+      <input className="search-box" type="search" value={query} onChange={handleChange}></input>
+      <div className={documents.length > 0 ? "list-view" : "list-view-none"}>
+      {documents.map((doc,index) => {
+        if(documents.length === index + 1) {
+          return <div ref={lastDocument} key={doc}> {doc} </div>
+        }
+        return (
+          <div className="document" key={doc}> {doc} </div>
+        )
+      })}
+      </div>
+      <div>{ isLoading && <img src={logo} className="App-logo" alt = "Loading..."></img>}</div>
+      <div>{error && "Something went wrong... Please try again!"}</div>
     </div>
+
   );
 }
 
---
+++ src/Hooks/useDocumentList.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useDocumentList(pageNum, query) {
+    const [isLoading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [documents, setDocuments] = useState([]);
+    const [hasMoreDocs, setHasMoreDocs] = useState(false);
+
+    useEffect(() => {
+        setDocuments([]);
+    },[query]);
+
+    useEffect(() => {
+        if (!query) return;
+        setLoading(true);
+        setError(false);
+        let cancel;
+        axios({
+            method: "GET",
+            url: "http://openlibrary.org/search.json",
+            params: {q: query, page: pageNum},
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then((res) => {
+            setDocuments(prevDocs => {
+                return [...new Set([...prevDocs, ...res.data.docs.map(doc => doc.title)])]
+            });
+            setLoading(false);
+            setHasMoreDocs(res.data.docs.length > 0);
+        }).catch(e => {
+            if(axios.isCancel(e)) return;
+            setError(true);
+        });
+        return () => cancel();
+    }, [pageNum, query]);
+
+    return{
+        documents, hasMoreDocs, error, isLoading
+    };
+}
+
+export default useDocumentList;
